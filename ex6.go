790696aa9c0810fee6a3f288952269bfe68ea989
LeonardoVergutz <legolodas@gmail.com>
2023-06-05 20:35:51 -0300

    Soluções

--- ex6.go
+++ ex6.go
@@ -1,25 +1,31 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 )
 
-func main(){
+func main() {
 
-	var numeros [10]int {10,20,30,40,50,60,70,80,90,100}
+	var numeros = [10]int{10, 20, 30, 40, 50, 60, 70, 80, 90, 95}
 	var check int
 
 	fmt.Print("Digite um número:")
 	fmt.Scan(&check)
 
-
-	if range numeros = check {
-		fmt.Print("O Array contém o número informado!")
-	}else{
-		fmt.Print("O Array não contém o número informado!")
+	encontrado := false
+	for _, num := range numeros {
+		if num == check {
+			encontrado = true
+			break
+		}
 	}
 
-	fmt.Println("O numero inserido:",check)
-	fmt.Print("Array",numeros)
+	if encontrado {
+		fmt.Println("O Array contém o número informado!")
+	} else {
+		fmt.Println("O Array não contém o número informado!")
+	}
+
+	fmt.Println("O numero inserido:", check)
+	fmt.Print("Array", numeros)
 }
